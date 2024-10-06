741596705d98eef2488527529360affd9a543ee8
Piyush <pshtiwari0@gmail.com>
2024-10-06 23:18:41 +0530

    Admin Page complete, teacher, student, parent page, teacher list page completed

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -18,12 +18,12 @@ export default function DashboardLayout({
             className="flex items-center  justify-center lg:justify-start gap-2"
           >
             <Image src="/logo.png" alt="logo" width={32} height={32} />
-            <span className="hidden lg:block">SchooLama</span>
+            <span className="hidden lg:block font-bold">SchooLama</span>
           </Link>
           <Menu />
         </div>
         {/* RIGHT */}
-        <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA]">
+        <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-y-scroll flex flex-col">
           <Navbar />
           {children}
         </div>
---
+++ src/components/BigCalender.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { Calendar, momentLocalizer, View, Views } from "react-big-calendar";
+import moment from "moment";
+import { calendarEvents } from "@/lib/data";
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import { useState } from "react";
+
+const localizer = momentLocalizer(moment);
+
+const BigCalender = () => {
+  const [view, setView] = useState<View>(Views.WORK_WEEK);
+
+  const handleOnChange = (selectedView: View) => {
+    setView(selectedView);
+  };
+  return (
+    <Calendar
+      localizer={localizer}
+      events={calendarEvents}
+      startAccessor="start"
+      endAccessor="end"
+      views={["work_week", "day"]}
+      view={view}
+      onView={handleOnChange}
+      style={{ height: "98%" }}
+      // Limit view to 8am - 5pm
+      min={new Date(2024, 1, 0, 8, 0, 0)}
+      max={new Date(2024, 1, 0, 17, 0, 0)}
+    />
+  );
+};
+export default BigCalender;
