ba0d24039c2f3851ed9ae0e293b6ac1083f1e20e
Piyush <pshtiwari0@gmail.com>
2025-02-09 17:37:07 +0530

    Implement query on admin page, and use component inside server side containers

--- src/components/EventCalendar.tsx
+++ src/components/EventCalendar.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import Image from "next/image";
-import { useState } from "react";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
 
@@ -32,29 +32,15 @@ const events = [
 
 const EventCalendar = () => {
   const [value, onChange] = useState<Value>(new Date());
-  return (
-    <div className="bg-white rounded-md p-4">
-      <Calendar value={value} onChange={onChange} />
-      <div className="flex items-center justify-between">
-        <h1 className="text-xl font-semibold my-4">Events</h1>
-        <Image src="/moreDark.png" alt="" width={20} height={20} />
-      </div>
-      <div className="flex flex-col gap-4">
-        {events.map((event) => (
-          <div
-            className="p-5 rounded-md border-2 border-gray-100 border-t-4 odd:border-t-lamaSky even:border-t-lamaPurple"
-            key={event.id}
-          >
-            <div className="flex justify-between items-center">
-              <h1 className="font-semibold text-gray-600">{event.title}</h1>
-              <span className="text-gray-300 text-xs">{event.time}</span>
-            </div>
-            <p className="mt-2 text-gray-500 text-sm">{event.description}</p>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+  const router = useRouter();
+
+  useEffect(() => {
+    if (value instanceof Date) {
+      router.push(`?date=${value}`);
+    }
+  }, [value, router]);
+
+  return <Calendar value={value} onChange={onChange} />;
 };
 
 export default EventCalendar;
---
+++ src/components/EventCalendarContainer.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import EventList from "./EventList";
+import EventCalendar from "./EventCalendar";
+import Image from "next/image";
+
+const EventCalendarContainer = async ({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | undefined };
+}) => {
+  const { date } = searchParams;
+
+  return (
+    <div className="bg-white rounded-md p-4">
+      <EventCalendar />
+      <div className="flex items-center justify-between">
+        <h1 className="text-xl font-semibold my-4">Events</h1>
+        <Image src="/moreDark.png" alt="" width={20} height={20} />
+      </div>
+      <div className="flex flex-col gap-4">
+        <EventList dateParam={date} />
+      </div>
+    </div>
+  );
+};
+
+export default EventCalendarContainer;
